61d1756ff6ca13cc42e0474827d47b7d30dd3d46
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-11 19:59:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,27 +1,27 @@
 class Shoe
 attr_accessor :color, :size, :material, :condition
   def initialize(brand)
-    @brand=brand 
- end 
- def brand 
+    @brand=brand
+ end
+ def brand
    @brand
  end
 
-end 
+end
 
 # class Book
 #   attr_accessor :author, :page_count, :genre
-#  
+#
 #   def initialize(title)
 #     @title = title
 #   end
-#  
+#
 #   def title
 #     @title
 #   end
-#  
+#
 #   def turn_page
 #     puts "Flipping the page...wow, you read fast!"
 #   end
-#  
+#
 # end
